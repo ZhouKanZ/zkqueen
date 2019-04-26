5944a824e58c1752f18c97baf5250fe5794286fc
zhoukan <1259183297@qq.com>
2019-04-26 10:47:11 +0800

    添加mvp基本框架，后期需要与RxlifeCycle结合在一起

--- app/src/main/java/comzkqueen/customview/MultiTypeActivity.java
+++ app/src/main/java/comzkqueen/customview/MultiTypeActivity.java
@@ -5,7 +5,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import com.zkqueen.diffname.Config;
 
 /**
  * @author : zk
@@ -18,6 +17,6 @@ public class MultiTypeActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.multi_type_activity);
-        ((TextView)findViewById(R.id.tv_multi_port)).setText(Config.PORT);
+        ((TextView)findViewById(R.id.tv_multi_port)).setText("port");
     }
 }
